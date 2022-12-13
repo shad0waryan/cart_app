c7ffd9b033779618bf28d5878ddd0b5f8c40cee6
Aryan Agrawal <61613340+shad0waryan@users.noreply.github.com>
2022-12-13 13:15:34 +0530

    updated

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,17 +1,11 @@
-import { useState, useEffect, MutableRefObject } from "react";
+import { useState } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
-import { useRef } from "react";
 import Image from "next/image";
 
-import {
-  AiOutlineMinusCircle,
-  AiOutlinePlusCircle,
-  AiFillMinusCircle,
-  AiFillPlusCircle,
-  AiFillCloseCircle,
-} from "react-icons/ai";
+import { AiOutlineMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+import { Disclosure } from "@headlessui/react";
 const Navbar = ({
-  key,
+  key1,
   cart,
   addToCart,
   removeFromCart,
@@ -19,42 +13,20 @@ const Navbar = ({
   subTotal,
 }) => {
   const [timeOut, setTimeOut] = useState(false);
-
-  const toggleCart = () => {
-    if (ref.current.classList.contains("translate-x-full")) {
-      ref.current.classList.remove("translate-x-full");
-      ref.current.classList.add("translate-x-0");
-    } else if (!ref.current.classList.contains("translate-x-full")) {
-      ref.current.classList.remove("translate-x-0");
-      ref.current.classList.add("translate-x-full");
-    }
-  };
-  const ref = useRef() as MutableRefObject<HTMLDivElement>;
   return (
-    <div className="bg-black justify-between flex items-center p-3 sticky  z-10 shadow-md">
-      <div className=" text-white item text-2xl flex items-center">
-        <h1 className="mx-3">Store</h1>
-      </div>
-      <div
-        onClick={toggleCart}
-        className="flex flex-row cursor-pointer bg-slate-700  px-4 py-2 rounded-lg item items-center justify-center  place-content-end text-white text-2xl hover:scale-125 shadow-sm shadow-slate-400"
-      >
-        <Image src="/carts.ico" alt="" height={32} width={32}/>
-        <span className=" ml-2">Cart</span>
-      </div>
-      <div
-        ref={ref}
-        className="h-[100vh] rounded-lg sideCart absolute top-0 right-0 bg-amber-200 p-10 transform transition-transform translate-x-full"
-      >
-        <span className="rounded p-2 font-bold flex flex-row items-center">
-          <span className="mr-8">Timer:</span>
+    <div className="bg-black justify-between flex items-center py-1 p-3 sticky z-15 top-0 z-10 w-full shadow-md shadow-gray-300 box-shadow-md">
+      <div className=" text-white  flex flex-row items-center">
+        <span className="place-content-start">
+          <h1 className="mx-3  text-2xl ">Store</h1>
+        </span>
+        <span className=" rounded item p-2 font-bold flex flex-row items-center text-white">
           <CountdownCircleTimer
             onComplete={() => setTimeOut(true)}
-            key={key}
+            key={key1}
             isPlaying
-            size={80}
+            size={70}
             duration={600}
-            colors="#e0bc2b"
+            colors="#eec41b"
           >
             {({ remainingTime }) => {
               const minutes = Math.floor((remainingTime % 3600) / 60);
@@ -64,102 +36,90 @@ const Navbar = ({
             }}
           </CountdownCircleTimer>
         </span>
-        <h2 className="font-extrabold text-xl mt-3">Your Cart</h2>
-        <span
-          onClick={toggleCart}
-          className="absolute top-3 right-4 cursor-pointer text-xl"
-        >
-          <AiFillCloseCircle />
-        </span>
-        <ol>
-          <li className="font-bold">
-            <div className="item flex my-5 items-center">
-              <div className="w-1/3 text-xs">Items</div>
-              <div className="w-1/3 text-xs">Price</div>
-              <div className="w-1/3 text-xs">Quantity</div>
-            </div>
-          </li>
-          {Object.keys(cart).length == 0 && (
-            <div className="font-semibold my-4"> Cart is Empty!</div>
-          )}
-        </ol>
-        <ol>
-          {Object.keys(cart).map((k) => {
-            return (
-              <li key={k}>
+      </div>
+      <Disclosure>
+        <Disclosure.Button className="absolute right-8 inline-flex items-center peer justify-center rounded-md text-gray-800 hover:bg-gray-900 hover:text-white ">
+          <div className="flex flex-row cursor-pointer bg-slate-400  px-4 py-2 rounded-lg item items-center justify-center  place-content-end text-white text-2xl hover:scale-125 shadow-sm shadow-slate-100 box-shadow-md">
+            <Image src="/carts.ico" alt="" height={32} width={32} />
+            <span className="ml-2">Cart</span>
+          </div>
+        </Disclosure.Button>
+        <Disclosure.Panel>
+          <div className="h-max mt-[84px] right-4 shadow-md drop-shadow-xl shadow-gray-500 rounded-lg sideCart absolute top-0 bg-amber-200 p-10 peer-focus:left-0 ">
+            <h2 className="font-extrabold text-xl mt-3">Your Cart</h2>
+            <ol>
+              <li className="font-bold">
                 <div className="item flex my-5 items-center">
-                  <div className="text-xs w-1/3">{cart[k].name}</div>
-                  <div className="w-1/3 text-xs">{cart[k].price}</div>
-                  <div className="w-1/3 text-xl flex items-center ">
-                    <AiOutlineMinusCircle
-                      onClick={() => {
-                        removeFromCart(
-                          k,
-                          1,
-                          cart[k].price,
-                          cart[k].name,
-                          cart[k].discount
-                        );
-                      }}
-                      className="mx-2 cursor-pointer text-4xl"
-                    />
-                    <span className="text-sm">{cart[k].qty}</span>
-                    <AiFillPlusCircle
-                      onClick={() => {
-                        addToCart(
-                          k,
-                          1,
-                          cart[k].price,
-                          cart[k].name,
-                          cart[k].discount
-                        );
-                      }}
-                      className="mx-2 cursor-pointer text-4xl"
-                    />
-                  </div>
+                  <div className="w-1/3 text-xs">Items</div>
+                  <div className="w-1/3 text-xs">Price</div>
+                  <div className="w-1/3 text-xs">Quantity</div>
                 </div>
               </li>
-            );
-          })}
-        </ol>
-        <div>SubTotal: {subTotal}</div>
-        <div className="flex flex-row mb-5">
-          <button
-            onClick={() => {
-              return (
-                <ol>
-                  {Object.keys(cart).map((k) => {
-                    return (
-                      <ol>
-                        <li key={k}>
-                          <div className="item flex my-5 items-center">
-                            <div className="text-xs w-1/3">{cart[k].name}</div>
-                            <div className="w-1/3 text-xs">{cart[k].price}</div>
-                            <div className="w-1/3 text-xl  ">
-                              <span className="text-sm">{cart[k].qty}</span>
-                            </div>
-                          </div>
-                        </li>
-                      </ol>
-                    );
-                  })}
-                </ol>
-              );
-            }}
-            disabled={Object.keys(cart).length == 0 || timeOut}
-            className="disabled:opacity-50 flex mt-16 text-white bg-purple-600 border-0 py-2 px-4 focus:outline-none hover:bg-purple-700 rounded text-sm"
-          >
-            Checkout
-          </button>
-          <button
-            disabled={Object.keys(cart).length == 0}
-            onClick={clearCart}
-            className="disabled:opacity-50 flex mt-16 ml-3 text-white bg-purple-600 border-0 py-2 px-4 focus:outline-none hover:bg-purple-700 rounded text-sm"
-          >
-            Clear Cart
-          </button>
-        </div>
-      </div>
+              {Object.keys(cart).length == 0 && (
+                <div className="font-semibold my-4"> Cart is Empty!</div>
+              )}
+            </ol>
+            <ol>
+              {Object.keys(cart).map((k) => {
+                return (
+                  <li key={k}>
+                    <div className="item flex my-5 items-center">
+                      <div className="text-xs w-1/3">{cart[k].name}</div>
+                      <div className="w-1/3 ml-5 text-xs">{cart[k].price}</div>
+                      <div className="w-1/3  text-xl flex items-center ">
+                        <AiOutlineMinusCircle
+                          onClick={() => {
+                            removeFromCart(
+                              k,
+                              1,
+                              cart[k].price,
+                              cart[k].name,
+                              cart[k].discount
+                            );
+                          }}
+                          className="mx-2 cursor-pointer text-4xl"
+                        />
+                        <span className="text-sm">{cart[k].qty}</span>
+                        <AiFillPlusCircle
+                          onClick={() => {
+                            addToCart(
+                              k,
+                              1,
+                              cart[k].price,
+                              cart[k].name,
+                              cart[k].discount
+                            );
+                          }}
+                          className="mx-2 cursor-pointer text-4xl"
+                        />
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ol>
+            <div>SubTotal: {subTotal}</div>
+            <div className="flex flex-row mb-5">
+              <button
+                onClick={() => {
+                  console.log(cart);
+                }}
+                disabled={Object.keys(cart).length == 0 || timeOut}
+                className="disabled:opacity-50 flex mt-16 text-white bg-purple-600 border-0 py-2 px-4 focus:outline-none hover:bg-purple-700 rounded text-sm"
+              >
+                Checkout
+              </button>
+              <button
+                disabled={Object.keys(cart).length == 0}
+                onClick={clearCart}
+                className="disabled:opacity-50 flex mt-16 ml-3 text-white bg-purple-600 border-0 py-2 px-4 focus:outline-none hover:bg-purple-700 rounded text-sm"
+              >
+                Clear Cart
+              </button>
+            </div>
+          </div>
+        </Disclosure.Panel>
+      </Disclosure>
     </div>
   );
 };
